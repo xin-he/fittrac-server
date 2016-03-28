cef5caa20a4562026f993f9448c815a963de2805
xin-he <xinhe@ufl.edu>
2016-03-28 19:56:21 -0400

    login

---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+  'secret': 'lobster897',
+  'database': 'mongodb://fittrac:lobster897@ds025409.mlab.com:25409/fittrac'
+};
---
+++ server.js
@@ -0,0 +1,56 @@
+var bodyParser 	= require('body-parser');
+var express 		= require('express');
+var helmet 			= require('helmet');
+var jwt         = require('jwt-simple');
+var mongoose 		= require('mongoose');
+var morgan 			= require('morgan');
+var passport		= require('passport');
+
+
+var index 	= require('./routes/index');
+var config 	= require('./config/database');
+var signup 	= require('./routes/signup');
+var auth 		= require('./routes/authenticate');
+var users 	= require('./routes/users');
+
+var app = express();
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// Parse application/json
+app.use(bodyParser.json());
+app.use(helmet());
+// HTTP request logger
+app.use(morgan('combined'))
+app.use(passport.initialize());
+
+// Connect to the howdiy_db
+mongoose.connect(config.database);
+var db = mongoose.connection;
+
+// For cross-domain requests
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Authorization"
+  );
+  next();
+});
+
+// Set port number
+app.set('port', process.env.PORT || 3001);
+
+app.get('/', function(req, res) {res.send('Use /api/');});
+app.use('/api/', index);
+app.use('/api/signup', signup)
+app.use('/api/auth', auth);
+app.use('/api/u', users);
+
+app.listen(app.get('port'), function() {
+  console.log('Express started! Running on port ' + app.get('port') + '. Press CTRL-C to terminate');
+});
