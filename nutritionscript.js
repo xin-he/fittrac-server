4725bcac502cd50263c7386784f8f300b3cf1e8c
xin-he <xinhe094@gmail.com>
2016-03-31 20:48:03 -0400

    nutrition image grabbing update

--- nutritionscript.js
+++ nutritionscript.js
@@ -1,10 +1,10 @@
-var bodyParser 	= require('body-parser');
-var mongoose 		= require('mongoose');
+var bodyParser  = require('body-parser');
+var mongoose    = require('mongoose');
 var http = require('http');
 var https = require('https');
 
-var index 	= require('./routes/index');
-var config 	= require('./config/database');
+var index   = require('./routes/index');
+var config  = require('./config/database');
 var nutrition = require('./models/nutrition');
 
 // Connect to the howdiy_db
@@ -35,26 +35,43 @@ var callback = function(response) {
 //http.request("http://wger.de/api/v2/ingredient.json/?limit=8324", callback).end();
 
 //getting images and putting into mongo
+String.prototype.replaceAll = function(str1, str2, ignore) {
+    return this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),(ignore?"gi":"g")),(typeof(str2)=="string")?str2.replace(/\$/g,"$$$$"):str2);
+} 
+
 nutrition.getAllNutrition(function(err, nutritionItems) {
   console.log(nutritionItems.length);
-  // for (i = 0; i < nutritionItems.length; i++) {
-    // if (nutritionItems[i].image !== "TEST" || nutritionItems[i].image.length > 4) {
-      // nutritionItems.splice(i,1);
-    // }
-  // }
+  var otherArray = [];
+  for (i = 0; i < nutritionItems.length; i++) {
+    // nutrition.updateNutrition({name : nutritionItems[i].name}, {image : "TEST"}, function(err, updatedNutrition){
+    //   if (err) {
+    //     console.log(err)
+    //   }
+    //   else {
+    //     //console.log(count + " " + fullname + " TEST2\n");
+    //   }
+    // });
+    if (nutritionItems[i].image === "TEST") {
+      otherArray.push(nutritionItems[i]);
+    }
+  }
+  nutritionItems = otherArray;
+  console.log(nutritionItems.length);
+  console.log(otherArray.length);
   var finished = 0;
-  var count = 775;
+  var count = 0;
   var getImages = function() {
-    link = "https://www.googleapis.com/customsearch/v1?q=" + nutritionItems[count].name.substring(0,Math.min(nutritionItems[count].name.length, 31)) + "&searchType=image&imgSize=medium&key=AIzaSyB6gvmN9BmNYW0tqw0D-pWfWF6G3P0Xn_E&cx=015241435869049310049%3A6veyqp-y3kq";
+    link = "https://www.googleapis.com/customsearch/v1?q=" + nutritionItems[count].name.replaceAll("%", "") + "&searchType=image&imgSize=medium&key=AIzaSyB6gvmN9BmNYW0tqw0D-pWfWF6G3P0Xn_E&cx=015241435869049310049:6veyqp-y3kq";
+    var fullname = nutritionItems[count].name;
+    //console.log(test);
+    //console.log(nutritionItems[count].name.replaceAll("%", ""));
     https.request(link, function(response) {
       var str = "";
       response.on('data', function(d) {
         str += d;
       });
       
-      var test = nutritionItems[count].name;
       response.on('end', function() {
-        //str = str.replace(/<\/?[^>]+(>|$)/g, "");
         //console.log(str);
         if (str.indexOf("<") > -1) {
           console.log(str);
@@ -62,26 +79,39 @@ nutrition.getAllNutrition(function(err, nutritionItems) {
         }
         else {
           var data = JSON.parse(str);
-          console.log(data);
-          //console.log(data.queries.request[0].searchTerms);
-          //console.log(data.items[0].link);
-          if (data.queries.request[0].searchTerms && data.items[0].link){
-            nutrition.updateNutrition({name : test}, {image : data.items[0].link}, function(err, updatedNutrition){
+          //console.log(data);
+          if (data.queries && data.queries.request && data.items){
+            console.log(data.queries.request[0].searchTerms);
+            console.log(data.items[0].link);
+            nutrition.updateNutrition({name : fullname}, {image : data.items[0].link}, function(err, updatedNutrition){
               if (err) {
                 console.log(err)
               }
               else {
-                console.log(count + " " + test);
+                console.log(count + " " + fullname + "\n");
               }
             });
+          }
+          else if (!data.error){
+            nutrition.updateNutrition({name : fullname}, {image : "http://i.imgur.com/uIzBRDe.png"}, function(err, updatedNutrition){
+              if (err) {
+                console.log(err)
+              }
+              else {
+                console.log(count + " " + fullname + " TEST2\n");
+              }
+            });
+          }
+          else {
+            console.log(data.error);
           }
         }
       });
     }).end();
   };
   
-  setInterval(function(){ 
+  setInterval(function(){
     getImages();
     count++;
-  }, 1500);
+  }, 2000);
 });
